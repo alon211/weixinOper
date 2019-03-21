3ab0ad289571720860a5b0a6a40e5457beccbfad
alon211 <306068447@qq.com>
2019-03-21 15:33:57 +0800

    增加github上传失败后处理

--- mythread.py
+++ mythread.py
@@ -7,56 +7,88 @@ PATH=r'C:\Users\admin\PycharmProjects\weixinOper\log'
 data_queue=queue.Queue()
 uploadedfiles=set(read_uploadedfile('uploadedfiles.txt'))
 con=threading.Condition()
+isEmpty=True
 print(uploadedfiles)
 # 模拟生成新的文件
 def product(*args):
     data,exit_flag=args
     thread_name=threading.current_thread().name
     print(f'{thread_name}: start!')
-    tmp={}
-    if con.acquire():
-        while not exit_flag:
-            # time.sleep(1)
-            if len(tmp)==0:
-                print(f'{thread_name}:no new file')
-                tmp=get_newlogset(PATH,uploadedfiles)
-                con.wait()
-                continue
-            i=tmp.pop()
-            print(f'{thread_name} is putting data on the queue')
-            # print(i)
-            uploadedfiles.add(i)
-            data.put(i)
-            print(f'{thread_name} have put data on the queue')
-            print(f'{thread_name}- data size :{data.qsize()}')
-            con.notify()
-            con.wait()
+    uploading_files={}
+
+    while not exit_flag:
+        # time.sleep(1)
+        con.acquire()
+        global uploadedfiles
+        if len(uploading_files)==0:
+            # lock.acquire()
+            print(f'{thread_name}:no new file')
+            uploading_files=get_newlogset(PATH,uploadedfiles)
+            # print(uploadedfiles)
+            # lock.release()
+            continue
+        # lock.acquire()
+        i=uploading_files.pop()
+        print(f'{thread_name} is putting data on the queue')
+        # print(i)
+        uploadedfiles.add(i)
+        data.put(i)
+        print(f'{thread_name} have put data on the queue')
+        print(f'{thread_name}- data size :{data.qsize()}')
+        # lock.release()
+        global isEmpty
+        isEmpty=False
+        con.notify()
+        con.wait()
 # 模拟上传文件
 def consumer(*args):
     data,=args
     thread_name=threading.current_thread().name
     print(f'{thread_name}: start!')
     isuploaded=True
-    if con.acquire():
-        while True:
-            try:
+    status=None
+    while True:
+        # lock.acquire()
+        try:
+            con.acquire()
 
-                # lock.acquire()
+            global uploadedfiles,isEmpty
+            if not isEmpty:
                 value=data.get(timeout=1)
-                print(f'{thread_name} get a vaule:{value} in the queue')
-                print(f'{thread_name}: uploading to github')
-                print(f'{git_oper(value)}')
-                save_uploadedfile('uploadedfiles.txt',value)
+            # print(f'value={value}')
+            print(f'{thread_name} get a vaule:{value} in the queue')
+            # print(f'{thread_name}: uploading to github')
+            status=git_oper(value)[0]
+            print(f'-------------------status:{status}')
+            # 传送失败,重新放入队列
+            if not status:
+                # lock.acquire()
+                print(f'fail to upload {value}')
+                # data.put(value)
+                uploadedfiles.remove(value)
+                con.notify()
+                con.wait()
                 # lock.release()
-                print(f'{thread_name}- data size :{data.qsize()}')
-                con.notify()
-                con.wait()
-            except :
-                print(f'the queue is empty,{thread_name} is waiting a value ')
-                con.notify()
-                con.wait()
+                time.sleep(10)
+                continue
+            # lock.acquire()
+            save_uploadedfile('uploadedfiles.txt',value)
+            print(f'{thread_name}- data size :{data.qsize()}')
+            # lock.release()
+            con.notify()
+            con.wait()
+        except Exception as e:
+            isEmpty=True
+            print(f'Error:{e}')
+            # print(status)
+            # print(value)
+            print(f'the queue is empty,{thread_name} is waiting a value ')
+            # lock.release()
+            con.notify()
+            con.wait()
+        # lock.release()
 
-lock=threading.Lock()
+lock=threading.RLock()
 exit_flag=False
 thread_lock=threading.Lock()
 thread_prodcut=threading.Thread(target=product,name='product',args=(data_queue,exit_flag))
