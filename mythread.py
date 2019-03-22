91e2cb581e329dbc97a9c93a1e9558b12cb6c275
alon211 <306068447@qq.com>
2019-03-22 20:18:10 +0800

    异步线程实验

--- mythread.py
+++ mythread.py
@@ -17,17 +17,16 @@ def product(*args):
     uploading_files={}
 
     while not exit_flag:
-        # time.sleep(1)
-        con.acquire()
+
+        # con.acquire()
         global uploadedfiles
         if len(uploading_files)==0:
             # lock.acquire()
             print(f'{thread_name}:no new file')
             uploading_files=get_newlogset(PATH,uploadedfiles)
-            # print(uploadedfiles)
-            # lock.release()
+
             continue
-        # lock.acquire()
+
         i=uploading_files.pop()
         print(f'{thread_name} is putting data on the queue')
         # print(i)
@@ -38,8 +37,8 @@ def product(*args):
         # lock.release()
         global isEmpty
         isEmpty=False
-        con.notify()
-        con.wait()
+        # con.notify()
+        # con.wait()
 # 模拟上传文件
 def consumer(*args):
     data,=args
@@ -50,43 +49,61 @@ def consumer(*args):
     while True:
         # lock.acquire()
         try:
-            con.acquire()
+            # con.acquire()
 
             global uploadedfiles,isEmpty
+            # 队列是否为空
             if not isEmpty:
                 value=data.get(timeout=1)
             # print(f'value={value}')
             print(f'{thread_name} get a vaule:{value} in the queue')
             # print(f'{thread_name}: uploading to github')
-            status=git_oper(value)[0]
+            save_uploadedfile('uploadedfiles.txt',value)
+            print(f'{thread_name}- data size :{data.qsize()}')
+
+            # con.notify()
+            # con.wait()
+
+            t=git_oper(value)
+            # con.acquire()
+            status=t[0]
+            errcode=t[1]
             print(f'-------------------status:{status}')
-            # 传送失败,重新放入队列
+            # 传送失败,重新放入队列,
+            # 其实可以不考虑的，但正常流程操作应该考虑，这是由于git本来的机制你push时就算
+            # 断网，之后联网后他也会自动push的
             if not status:
                 # lock.acquire()
                 print(f'fail to upload {value}')
-                # data.put(value)
-                uploadedfiles.remove(value)
-                con.notify()
-                con.wait()
-                # lock.release()
-                time.sleep(10)
+                print(f'errcode:{errcode}')
+                # github已经上传相同的文件而且没有变化就会报errcode，重复上传
+                if errcode==1:
+                    uploadedfiles.add(value)
+                    save_uploadedfile('uploadedfiles.txt',value)
+
+                else:
+                    uploadedfiles.remove(value)
+                # con.notify()
+                # con.wait()
+
+                time.sleep(1)
                 continue
-            # lock.acquire()
-            save_uploadedfile('uploadedfiles.txt',value)
-            print(f'{thread_name}- data size :{data.qsize()}')
-            # lock.release()
-            con.notify()
-            con.wait()
+
+            # save_uploadedfile('uploadedfiles.txt',value)
+            # print(f'{thread_name}- data size :{data.qsize()}')
+
+            # con.notify()
+            # con.wait()
         except Exception as e:
             isEmpty=True
             print(f'Error:{e}')
             # print(status)
             # print(value)
             print(f'the queue is empty,{thread_name} is waiting a value ')
-            # lock.release()
-            con.notify()
-            con.wait()
-        # lock.release()
+
+            # con.notify()
+            # con.wait()
+
 
 lock=threading.RLock()
 exit_flag=False
